16565a099a2fd49bc4fc1b313f43c34d507cebab
Kros-media <mateusz.krosmedia@gmail.com>
2024-01-10 22:36:32 +0100

    fix7

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose'); // Dodaj ten import
+const MongoStore = require('connect-mongo'); 
 
 const { connectToDatabase, getUsersCollection } = require('./modules/db');
 const articleController = require('./modules/articleControler');
